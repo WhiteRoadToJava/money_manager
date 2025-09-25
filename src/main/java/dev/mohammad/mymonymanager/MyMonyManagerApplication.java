95a3d7cd34b79928ade68fe5ab49251b94a7f9e4
Your Namemohammad <msaabbas6@gmail.cimm>
2025-09-25 22:46:13 +0200

    start project

---
+++ src/main/java/dev/mohammad/mymonymanager/MyMonyManagerApplication.java
@@ -0,0 +1,13 @@
+package dev.mohammad.mymonymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMonyManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMonyManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/mohammad/mymonymanager/MyMonyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mohammad.mymonymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMonyManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
