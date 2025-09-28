2f0ff3343298f7466305fef090601e26a2dbb199
Your Namemohammad <msaabbas6@gmail.cimm>
2025-09-28 09:50:42 +0200

    fail in authentacation

---
+++ src/main/java/dev/mohammad/mymonymanager/service/AppUserDetailsService.java
@@ -0,0 +1,31 @@
+package dev.mohammad.mymonymanager.service;
+
+import dev.mohammad.mymonymanager.entity.ProfileEntity;
+import dev.mohammad.mymonymanager.repository.ProfileRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+
+// security step 5
+@Service
+  @RequiredArgsConstructor
+public class AppUserDetailsService  implements UserDetailsService {
+    private final ProfileRepository profileRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+         ProfileEntity existingProfile = profileRepository.findByEmail(email)
+                 .orElseThrow(() -> new UsernameNotFoundException("profile not fond with email: " + email));
+         return User.builder()
+                 .username(existingProfile.getFullName())
+                 .password(existingProfile.getPassword())
+                 .authorities(Collections.emptyList())
+                 .build();
+    }
+}
