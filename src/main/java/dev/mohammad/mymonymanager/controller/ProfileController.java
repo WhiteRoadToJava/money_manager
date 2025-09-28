fff2fcfe5e2e237dc81003a7acee4447f08c3644
Your Namemohammad <msaabbas6@gmail.cimm>
2025-09-28 15:54:00 +0200

    scurity is done

--- src/main/java/dev/mohammad/mymonymanager/controller/ProfileController.java
+++ src/main/java/dev/mohammad/mymonymanager/controller/ProfileController.java
@@ -1,15 +1,20 @@
 package dev.mohammad.mymonymanager.controller;
 
 
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import dev.mohammad.mymonymanager.dbo.AuthDTO;
 import dev.mohammad.mymonymanager.dbo.ProfileDTO;
 import dev.mohammad.mymonymanager.service.ProfileService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -56,6 +61,23 @@ public class ProfileController {
         ProfileDTO profileDTO = profileService.getPublicProfile(null);
         return ResponseEntity.ok(profileDTO);
     }
+
+
+
+
+
+
+
+
+
+
+
+    @GetMapping("/test")
+    public String test(){
+        return "test successful";
+    }
+    
+    
 
 
 
